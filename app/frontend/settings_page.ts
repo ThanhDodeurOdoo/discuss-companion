ff9fc158ac50e2882569d76ca6122903b7e8e57c
ThanhDodeurOdoo <tso@odoo.com>
2026-04-12 13:21:57 +0200

    [IMP] app: add bug reporting button

--- app/frontend/settings_page.ts
+++ app/frontend/settings_page.ts
@@ -1,9 +1,16 @@
 import { Component, plugin, xml } from "@odoo/owl";
+import { openUrl } from "@tauri-apps/plugin-opener";
 import "./settings_page.css";
 import { AppPlugin } from "./app_plugin";
 import template from "./settings_page.xml?raw";
 
+const BUG_REPORTS_URL = "https://github.com/ThanhDodeurOdoo/discuss-companion/issues";
+
 export class SettingsPage extends Component {
     static template = xml`${template}`;
     app = plugin(AppPlugin);
+
+    openBugReports() {
+        return openUrl(BUG_REPORTS_URL);
+    }
 }
